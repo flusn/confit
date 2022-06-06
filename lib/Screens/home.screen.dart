830ca1f7a85dcb186082b8192ec8f65b037cd78d
Flusn <debus.florian@gmail.com>
2022-06-06 16:52:44 +0200

    Profilbild in Stammdaten und AppBar ergänzt

--- lib/Screens/home.screen.dart
+++ lib/Screens/home.screen.dart
@@ -1,15 +1,29 @@
 import "package:flutter/material.dart";
-
+import '../models/profil_image.dart';
 import '../templates/menu.drawer.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: const MenuDrawer(),
-      appBar: AppBar(title: const Text("Homescreen")),
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const [
+            Text("Homescreen"),
+            ProfilImageInAppBar(),
+          ],
+        ),
+      ),
     );
   }
 }
